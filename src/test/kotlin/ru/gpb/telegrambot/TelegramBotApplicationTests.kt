1099d77e7a512af5c60eccceae5ae74130faaf63
rojob <rojoblaco@yandex.ru>
2024-05-22 21:00:04 +0300

    Добавление MockBean BotConfig

--- src/test/kotlin/ru/gpb/telegrambot/TelegramBotApplicationTests.kt
+++ src/test/kotlin/ru/gpb/telegrambot/TelegramBotApplicationTests.kt
@@ -2,12 +2,16 @@ package ru.gpb.telegrambot
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.mock.mockito.MockBean
+import ru.gpb.telegrambot.config.BotConfig
 
 @SpringBootTest
 class TelegramBotApplicationTests {
 
+	@MockBean
+	lateinit var telegramBot: BotConfig
+
 	@Test
 	fun contextLoads() {
 	}
-
 }
