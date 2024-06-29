bf52f2489a0f50ebb586b8abd27989cd34bde444
rojob <rojoblaco@yandex.ru>
2024-06-29 14:30:16 +0300

    Поправленны тесты

--- src/main/kotlin/ru/gpb/telegrambot/commands/RegisterCommand.kt
+++ src/main/kotlin/ru/gpb/telegrambot/commands/RegisterCommand.kt
@@ -5,12 +5,13 @@ import org.telegram.telegrambots.extensions.bots.commandbot.commands.BotCommand
 import org.telegram.telegrambots.meta.api.objects.Chat
 import org.telegram.telegrambots.meta.api.objects.User
 import org.telegram.telegrambots.meta.bots.AbsSender
+import ru.gpb.telegrambot.client.MiddleServiceClient
 import ru.gpb.telegrambot.client.MiddleServiceRestClient
 import ru.gpb.telegrambot.uitl.createMessage
 
 @Component
 class RegisterCommand(
-    private val middleServiceClient: MiddleServiceRestClient,
+    private val middleServiceClient: MiddleServiceClient,
 ) : BotCommand("register", "register new user") {
 
     override fun execute(absSender: AbsSender, user: User, chat: Chat, arguments: Array<out String>) {
--- src/test/kotlin/ru/gpb/telegrambot/commands/RegisterCommandTest.kt
+++ src/test/kotlin/ru/gpb/telegrambot/commands/RegisterCommandTest.kt
@@ -1,22 +1,44 @@
 package ru.gpb.telegrambot.commands
+import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.mockito.ArgumentCaptor
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage
+import org.telegram.telegrambots.meta.api.objects.Chat
+import org.telegram.telegrambots.meta.api.objects.User
+import org.telegram.telegrambots.meta.bots.AbsSender
 import ru.gpb.telegrambot.client.MiddleServiceClientStub
 import kotlin.random.Random
 
 class RegisterCommandTest {
     private val middleServiceClient = MiddleServiceClientStub()
+    private val  registerCommand = RegisterCommand(middleServiceClient)
 
     @Test
     @DisplayName("Успешная регистрация пользователя")
     fun successRegisterUser() {
         val telegramUserId = Random.nextLong()
-        val userName = "pepega1"
+        val telegramUserName = "pepega1"
+        val chatId = 67890L
 
-        val result = middleServiceClient.registerUser(telegramUserId, userName)
+        val absSender: AbsSender =  mock()
+        val user = User().apply {
+            id = telegramUserId
+            userName = telegramUserName
+        }
+        val chat = Chat()
+        chat.id = chatId
 
-        assertEquals("Пользователь ${telegramUserId} успешно зарегестрирован", result)
+        registerCommand.execute(absSender, user, chat, emptyArray())
+        val argumentCaptor = ArgumentCaptor.forClass(SendMessage::class.java)
+        Mockito.verify(absSender).execute(argumentCaptor.capture())
+
+        val capturedMessage = argumentCaptor.value
+
+        assertEquals("Пользователь ${user.id} успешно зарегестрирован", capturedMessage.text)
     }
 
 }
